4d9a36afb654e78aedf069baa592cc03e4fe0404
JezzyDeves <17914687+JezzyDeves@users.noreply.github.com>
2023-02-11 15:27:14 -0500

    Adds basic counter page (#4)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,4 +5,12 @@
 	export let data: LayoutData;
 </script>
 
-<slot />
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
+
+<div
+	class="flex min-h-screen w-screen flex-col flex-wrap content-center items-center justify-center bg-slate-900 font-roboto"
+>
+	<slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
+
+	let visible = false;
+	onMount(() => {
+		visible = true;
+	});
+</script>
+
+{#if visible}
+	<h1
+		in:fade={{ duration: 2000 }}
+		class="mb-3 rounded-md bg-sky-700 p-2 text-center text-4xl font-bold text-sky-200"
+	>
+		Welcome to Svelte + SvelteKit 👋
+	</h1>
+	<a
+		href="/simple-counter"
+		in:fade={{ duration: 500, delay: 2000 }}
+		class="w-48 rounded-md bg-sky-800 p-3 text-center font-bold text-sky-200 transition-colors duration-300 hover:bg-sky-900"
+		>Let's Get Started!</a
+	>
+{/if}
